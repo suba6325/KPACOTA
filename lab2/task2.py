53673d3a440c4e05215f594e745066f802cf2a8a
suba <145760609+suba6325@users.noreply.github.com>
2024-10-31 18:48:23 +0300

    lab2

--- lab2/task2.py
+++ lab2/task2.py
@@ -1,14 +1,16 @@
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
-months = 10  # Количество месяцев, которое планируется протянуть без долгов
+months = 1  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.05  # Ежемесячный рост цен
 
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
 
 allspend = 0
+allsalary = 0
 
 for month in range(0, months):
     allspend += spend * (1.05 ** month)
+
 
 money_capital = round(allspend - months * salary)
 
