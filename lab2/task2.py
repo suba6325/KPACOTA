02003a32284f17e99dd503be294df8a26a788074
suba <145760609+suba6325@users.noreply.github.com>
2024-11-01 21:58:20 +0300

    Update task2.py

--- lab2/task2.py
+++ lab2/task2.py
@@ -1,3 +1,4 @@
+import math
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
@@ -10,7 +11,6 @@ allspend = 0
 for month in range(0, months):
     allspend += spend * (1.05 ** month)
 
-money_capital = round(allspend - months * salary)
+money_capital =  math.ceil(allspend - months * salary)
 
 print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
-# тут получается 25467,3, но я не понял как округлить до большего целого, но и не важно. Так ведь?
