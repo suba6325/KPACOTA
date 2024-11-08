ec0c65c79a0e43022e685dccf371b9a362849795
suba <145760609+suba6325@users.noreply.github.com>
2024-11-08 18:36:59 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,20 +1,17 @@
 # TODO Напишите функцию find_common_participants
 
-def find_common_participants(participants_first_group, participants_second_group, seporator=', '):
-    list_first = participants_first_group.split(seporator)
-    list_second = participants_second_group.split(seporator)
+def find_common_participants(participants_first_group, participants_second_group, seporator=','):
     list_similar = []
-    for i in range(len(list_first)):
-        for k in range(len(list_second)):
-            if list_first[i] == list_second[k]:
-                list_similar.append(list_first[i])
+    for i, surname_1 in enumerate(participants_first_group.split(seporator)):
+        for k, surname_2 in enumerate(participants_second_group.split(seporator)):
+            if surname_1 == surname_2:
+                list_similar.append(surname_1)
     return list_similar
 
 
-participants_first_group = "Иванов, Петров, Сидоров"
-participants_second_group = "Петров, Сидоров, Смирнов"
+participants_first_group = "Иванов,Петров,Сидоров"
+participants_second_group = "Петров,Сидоров,Смирнов"
 print(find_common_participants(participants_first_group, participants_second_group))
-
 
 # TODO Провеьте работу функции с разделителем отличным от запятой
 
